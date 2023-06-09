32ecd7d21579e0b2030c5770cff8e0d10df910a2
s <s@localhost.localdomain>
2023-06-09 16:56:42 +0700

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 
-import "./App.css";
-
 function App() {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
@@ -46,72 +44,180 @@ function App() {
     setSelectedItem(null);
   };
 
-  console.log(data, "data");
-  console.log(filteredData, "data filter");
   return (
     <>
-      {" "}
-      <div>
-        <h1>Website dengan React.js</h1>
-
-        <div>
-          <h2>Filter</h2>
-          <input
-            type="text"
-            placeholder="Cari berdasarkan nama"
-            onChange={(e) => filterData(null, e.target.value)}
-          />
-
-          <select onChange={(e) => filterData(e.target.value, null)}>
-            <option value="">Semua Kategori</option>
-            <option value="Duelist">Duelist</option>
-            <option value="Initiator">Initiator</option>
-            <option value="Controller">Controller</option>
-            <option value="Sentinel">Sentinel</option>
-          </select>
-        </div>
-
-        <div>
-          <h2>Jumlah Kategori yang Sama: {filteredData.length}</h2>
-        </div>
-
-        <div>
-          <h2>List Item</h2>
-          {filteredData.map((item) => (
-            <div key={item.id} onClick={() => handleItemClick(item)}>
-              <img src={item.thumbnail} alt={item.displayName} />
-              {item.displayName}
-            </div>
-          ))}
-        </div>
-
-        {selectedItem && (
-          <div>
-            <h2>Modal Detail</h2>
-            <div>
-              <img
-                src={selectedItem.thumbnail}
-                alt={selectedItem.displayName}
-              />
-              <p>Nama: {selectedItem.displayName}</p>
-              <p>Role: {selectedItem.role}</p>
-              <p>Deskripsi: {selectedItem.description}</p>
-              {selectedItem.video && (
-                <iframe
-                  width="560"
-                  height="315"
-                  src={selectedItem.video}
-                  title="Video Player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                ></iframe>
-              )}
-            </div>
-            <button onClick={closeModal}>Tutup</button>
+      <nav className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
+        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+          <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
+            <span className="font-semibold uppercase text-xl tracking-tight">
+              my Item
+            </span>
           </div>
-        )}
-      </div>
+          <div className="block lg:hidden ">
+            <button
+              id="nav"
+              className="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700"
+            >
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+          <div className="text-md font-bold text-blue-700 lg:flex-grow">
+            <span className="block mt-4 lg:inline-block lg:mt-0  px-4 py-2 rounded mr-2">
+              <select
+                className=" w-full items-center justify-between rounded bg-white p-2 ring-1 ring-gray-300"
+                onChange={(e) => filterData(e.target.value, null)}
+              >
+                <option value="">Semua Kategori</option>
+                <option value="Duelist">Duelist</option>
+                <option value="Initiator">Initiator</option>
+                <option value="Controller">Controller</option>
+                <option value="Sentinel">Sentinel</option>
+              </select>
+            </span>
+          </div>
+
+          <div className="relative mx-auto text-gray-600 lg:block hidden">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+              type="text"
+              placeholder="Cari berdasarkan nama"
+              onChange={(e) => filterData(null, e.target.value)}
+            />
+            <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">
+              <svg
+                className="text-gray-600 h-4 w-4 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                width="512px"
+                height="512px"
+              >
+                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-14 mx-auto">
+          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
+            Jumlah Kategori yang Sama: {filteredData.length}
+          </h1>
+          <div className="flex flex-wrap -m-4">
+            {filteredData.map((item) => (
+              <div key={item.id} className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={item.displayIcon}
+                    alt="blog"
+                    onClick={() => handleItemClick(item)}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                      {item.role}
+                    </h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                      {item.displayName}
+                    </h1>
+                    <p className="leading-relaxed mb-3">{item.description}</p>
+                    <div
+                      onClick={() => handleItemClick(item)}
+                      className="flex items-center flex-wrap "
+                    >
+                      <span className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                        Details
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {selectedItem && (
+        <>
+          {" "}
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    {selectedItem.displayName}
+                  </h3>
+
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={closeModal}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    {selectedItem.description}
+                  </p>
+
+                  {selectedItem.video && (
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={selectedItem.video}
+                      title="Video Player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  )}
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={closeModal}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
     </>
   );
 }
