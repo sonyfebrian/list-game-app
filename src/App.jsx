b6025742f864a26bd9e7fb803656d796fb20a9c9
s <s@localhost.localdomain>
2023-06-09 13:21:10 +0700

    integrasi API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://staging.ina17.com/data.json");
+      const jsonData = await response.json();
+      setData(jsonData);
+      setFilteredData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const filterData = (role, displayName) => {
+    let filtered = data;
+
+    if (role) {
+      filtered = filtered.filter((item) => item.role === role);
+    }
+
+    if (displayName) {
+      filtered = filtered.filter((item) =>
+        item.displayName.toLowerCase().includes(displayName.toLowerCase())
+      );
+    }
+
+    setFilteredData(filtered);
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const closeModal = () => {
+    setSelectedItem(null);
+  };
+
+  console.log(data, "data");
+  console.log(filteredData, "data filter");
   return (
     <>
+      {" "}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Website dengan React.js</h1>
+
+        <div>
+          <h2>Filter</h2>
+          <input
+            type="text"
+            placeholder="Cari berdasarkan nama"
+            onChange={(e) => filterData(null, e.target.value)}
+          />
+
+          <select onChange={(e) => filterData(e.target.value, null)}>
+            <option value="">Semua Kategori</option>
+            <option value="Duelist">Duelist</option>
+            <option value="Initiator">Initiator</option>
+            <option value="Controller">Controller</option>
+            <option value="Sentinel">Sentinel</option>
+          </select>
+        </div>
+
+        <div>
+          <h2>Jumlah Kategori yang Sama: {filteredData.length}</h2>
+        </div>
+
+        <div>
+          <h2>List Item</h2>
+          {filteredData.map((item) => (
+            <div key={item.id} onClick={() => handleItemClick(item)}>
+              <img src={item.thumbnail} alt={item.displayName} />
+              {item.displayName}
+            </div>
+          ))}
+        </div>
+
+        {selectedItem && (
+          <div>
+            <h2>Modal Detail</h2>
+            <div>
+              <img
+                src={selectedItem.thumbnail}
+                alt={selectedItem.displayName}
+              />
+              <p>Nama: {selectedItem.displayName}</p>
+              <p>Role: {selectedItem.role}</p>
+              <p>Deskripsi: {selectedItem.description}</p>
+              {selectedItem.video && (
+                <iframe
+                  width="560"
+                  height="315"
+                  src={selectedItem.video}
+                  title="Video Player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              )}
+            </div>
+            <button onClick={closeModal}>Tutup</button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
